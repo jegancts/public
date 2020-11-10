6289895aeb2389efbd62f934715296a1358c8085
Maharajan <123502@cognizant.com>
2020-11-10 17:12:36 +0000

    co

---
+++ routes/homepage.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const router = express.Router();
+
+router.post('/',(req, res) => {
+    res.render('publicusersearch/homepage')
+  });
+
+  router.get('/',(req, res) => {
+    res.render('publicusersearch/homepage')
+  });
+
+module.exports = router;
